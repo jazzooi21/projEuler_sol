13534bc54bc591c605d7213de5cc43c923104135
Jazz Ooi <38699175+jazzooi21@users.noreply.github.com>
2022-08-18 09:52:38 +0100

    Add files via upload

---
+++ isprime.py
@@ -0,0 +1,11 @@
+import math
+
+def isprime(p):
+    if p == 1 or p <= 0:
+        return False
+    elif p == 2:
+        return True
+    for i in range(2, int(math.sqrt(p))+1):
+        if p%i == 0:
+            return False
+    return True

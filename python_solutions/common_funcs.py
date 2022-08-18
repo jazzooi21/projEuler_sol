1ef2a8ffac3ff383bccf69b7d32be0356caf9407
Jazz Ooi <38699175+jazzooi21@users.noreply.github.com>
2022-08-18 17:27:59 +0800

    Update common_funcs.py

--- python_solutions/common_funcs.py
+++ python_solutions/common_funcs.py
@@ -10,3 +10,21 @@ def isprime(p):
         if p%i == 0:
             return False
     return True
+
+
+def return_primes_under_n(n):
+    primes = []
+    for i in range(1, n):
+        if isprime(i):
+            primes.append(i)
+    return primes
+
+
+def factors(num):
+    fac = set()
+    fac.add(1)
+    for i in range(2, int(math.sqrt(num))+1):
+        if num % i == 0:
+            fac.add(i)
+            fac.add(int(num / i))
+    return fac
